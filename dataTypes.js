730f2daf982e3f0188f656ce546804dbed03c968
Vikalp Soni <imvikalpsoni@gmail.com>
2026-03-27 18:12:09 +0530

    DataTypes

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" 
+/*
+this is used to treat all js code as newer version but doesn't need anymore because it happens automatically
+*/
+// [alert (3+3)] it will give output 6 in browser becuase js engine is in browser, it won't work in vs code. there is another method to it
+
+//Note :- Read Document on tc39.es it will give standards of js
+
+let name = "Vikalp"
+let age = 21
+let isloggedIn = true
+
+// number => 2^53
+// bigint => use in apps like fb, insta, stock market
+//null = standalone value
+// undefined (mostly will see)
+// symbol => use to find uniqueness
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object (Because it is either defect or this is the way)
+
